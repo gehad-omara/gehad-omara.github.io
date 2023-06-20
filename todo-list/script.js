a6ade0696b45aa52cc50e600ff48b7a5ec8879f7
gehad-omara <132614716+gehad-omara@users.noreply.github.com>
2023-06-20 21:16:37 +0300

    to do list

---
+++ todo-list/script.js
@@ -0,0 +1,50 @@
+/*getElementsByTagName could not be used with addEventListener, I don't know why*/
+
+const button = document.querySelector("button");
+const listContainer = document.getElementById("list-container");
+const list = document.getElementById("list");
+const textbox = document.getElementById("text");
+const restored = document.getElementById("restored");
+
+button.addEventListener("click", addTask);
+textbox.addEventListener('keypress', function (e){
+ if (e.key == "Enter") {
+    e.preventDefault();
+    addTask();
+ }
+ })
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName == "LI"){
+        e.target.classList.toggle("done");
+        save();
+    }
+    if (e.target.tagName == "I") {
+        e.target.parentElement.remove();
+        save();
+    }
+})
+function addTask(e) {
+    if(textbox.value.length != 0) { 
+        var task = document.createElement("li");
+         task.innerHTML = textbox.value;
+         list.appendChild(task);
+         var icon = document.createElement("i");
+         icon.setAttribute("class", "fa-regular fa-trash-can");
+         task.appendChild(icon);
+        //  var hr =document.createElement("hr");
+        //  list.appendChild(hr);
+         textbox.value = "";
+         save();
+        }
+         };
+
+function save () {
+    localStorage.setItem("data", list.innerHTML)
+}
+
+function restore () {
+    restored.innerHTML = localStorage.getItem("data");
+} 
+restore();
+
